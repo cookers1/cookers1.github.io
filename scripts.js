148a71728593e39045456f4579fd1736351bde1b
cookers1 <genericguythere@gmail.com>
2024-06-06 22:51:05 +0100

    Add modifier info

--- scripts.js
+++ scripts.js
@@ -36,13 +36,29 @@ function displayBattleStats(data) {
   const formattedDefense = data.defense.toLocaleString();
   const formattedDexterity = data.dexterity.toLocaleString();
   const formattedTotal = data.total.toLocaleString();
-
+  const effectiveStrength = (data.strength * (1 + data.strength_modifier / 100)).toLocaleString();
+  const effectiveSpeed = (data.speed * (1 + data.speed_modifier / 100)).toLocaleString();
+  const effectiveDefense = (data.defense * (1 + data.defense_modifier / 100)).toLocaleString();
+  const effectiveDexterity = (data.dexterity * (1 + data.dexterity_modifier / 100)).toLocaleString();
+  const effectiveTotal = ((data.dexterity * (1 + data.dexterity_modifier / 100)) 
+  + (data.defense * (1 + data.defense_modifier / 100)) + (data.speed * (1 + data.speed_modifier / 100)) 
+  + (data.strength * (1 + data.strength_modifier / 100))).toLocaleString();
   const statsHtml = `
-      <p>Strength: ${formattedStrength}</p>
-      <p>Speed: ${formattedSpeed}</p>
-      <p>Defense: ${formattedDefense}</p>
-      <p>Dexterity: ${formattedDexterity}</p>
-      <p>Total: ${formattedTotal}</p>
+      <p>Strength: ${formattedStrength}.    Effective Strength:${effectiveStrength} </p>
+      <p>Speed: ${formattedSpeed}.   Effective Speed: ${effectiveSpeed} </p>
+      <p>Defense: ${formattedDefense}.    Effective Defense: ${effectiveDefense} </p>
+      <p>Dexterity: ${formattedDexterity}.    Effective Dexterity: ${effectiveDexterity}</p>
+      <p>Total: ${formattedTotal}.    Effective Total: ${effectiveTotal} </p>
+      <p>Modifiers:</p>
+      <p>Strength modifier:${data.strength_modifier}%</p>
+      <ul>${data.strength_info.map(info => `<li>${info}</li>`).join('')}</ul>
+      <p>Speed modifier:${data.speed_modifier}%</p>
+      <ul>${data.speed_info.map(info => `<li>${info}</li>`).join('')}</ul>
+      <p>Defense modifier:${data.defense_modifier}%</p>
+      <ul>${data.defense_info.map(info => `<li>${info}</li>`).join('')}</ul>
+      <p>Dexterity modifier:${data.dexterity_modifier}%</p>
+      <ul>${data.dexterity_info.map(info => `<li>${info}</li>`).join('')}</ul>
+
   `;
   statsContainer.innerHTML = statsHtml;
 }  
